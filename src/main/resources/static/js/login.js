a375a57d56c60d0754894c359001a5334e4b2f6d
Froggo23 <ichoe27@student.kis.or.kr>
2024-01-09 21:39:53 +0900

    login error fixed

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -7,9 +7,10 @@ async function checkLogin() {
     const u = document.getElementById('username').value;
     const p = document.getElementById('password').value;
 
+    let response;
     try {
         // Send the POST request
-        const response = await fetch('/checkLogin', {
+        response = await fetch('/checkLogin', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -20,19 +21,19 @@ async function checkLogin() {
             })
         });
 
-        const responseBody = await response.text();
 
-        if (response.ok && responseBody === "success") {
-            document.cookie = `login_id=${u}; expires=${new Date(Date.now() + 3 * 60 * 1000).toUTCString()}; path=/`;
-            alert('Login succeeded!');
-            window.location.href = "/board";
-        } else {
-            alert('Login error. Please check your username and password again.');
-        }
 
     } catch (error) {
         alert('Network error. Please check your connection and try again.');
     }
+    const responseBody = await response.text();
 
+    if (response.ok && responseBody === "success") {
+        document.cookie = `login_id=${u}; expires=${new Date(Date.now() + 3 * 60 * 1000).toUTCString()}; path=/`;
+        alert('Login succeeded!');
+        window.location.href = "/board";
+    } else {
+        alert('Login error. Please check your username and password again.');
+    }
 
 };
